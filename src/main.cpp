d2c68f7178c08ce0fa726f104f92b9286bac3580
YSteinmetz <steinmetz@alunos.utfpr.edu.br>
2024-01-07 13:28:29 -0300

    new changes

--- src/main.cpp
+++ src/main.cpp
@@ -41,146 +41,341 @@ void setup() {
 
 void loop() {
 
-  char address, data;
+  static int operationMode = 1;
   
-  //Ask for the user input
+  //Ask for the register address
   /*
+    (D8 - D9 - D10 - D11)
     Options:
-      W. for wake up the max7219
-      S. for shutdown the max7219
+      0 - No-Op
+      1 - Digit 0
+      2 - Digit 1
+      3 - Digit 2
+      4 - Digit 3
+      5 - Digit 4
+      6 - Digit 5
+      7 - Digit 6
+      8 - Digit 7
+      9 - Decode Mode
+      A - Intensity
+      B - Scan Limit
+      C - Shutdown
+      F - Display Test
 
-      T. for test mode
-      N. for normal mode
-
-      M. to write the address and the data to send
-
-      O. to turn on all the leds
-      F. to turn off all the leds
-
-      D. no decode mode
-      I. for the minimum intensity
-      H. for the maximum intensity
-
-      L. to scan limit
+      
   */
   //send the command list to the user
-  Serial.println("");
-  Serial.println("");
-  Serial.println("Please select a option:");
-  Serial.println("W. for wake up the max7219");
-  Serial.println("S. for shutdown the max7219");
-  Serial.println("T. for test mode");
-  Serial.println("N. for normal mode");
-  Serial.println("M. to write the address and the data to send");
-  Serial.println("O. to turn on all the leds");
-  Serial.println("F. to turn off all the leds");
-  Serial.println("D. no decode mode");
-  Serial.println("I. for the minimum intensity");
-  Serial.println("H. for the maximum intensity");
-  Serial.println("L. to scan limit");
+  Serial.println("Select the register address:");
+  Serial.println("0 - No-Op");
+  Serial.println("1 - Digit 0");
+  Serial.println("2 - Digit 1");
+  Serial.println("3 - Digit 2");
+  Serial.println("4 - Digit 3");
+  Serial.println("5 - Digit 4");
+  Serial.println("6 - Digit 5");
+  Serial.println("7 - Digit 6");
+  Serial.println("8 - Digit 7");
+
+  Serial.println("9 - Decode Mode");
+  Serial.println("A - Intensity");
+  Serial.println("B - Scan Limit");
+  Serial.println("C - Shutdown");
+  Serial.println("F - Display Test");
+
+  Serial.println("O - Turn on all leds");
+  Serial.println("X - Turn off all leds");
 
 
   //read the user input
   while(!Serial.available());
-  char option = Serial.read();
+  char selectedAddr = Serial.read();
+
+  if(selectedAddr == 'O' || selectedAddr == 'o'){
+    //Turn on all leds
+    sendMessage("Turn on all leds");
+    /*
+    transferData(0x0F, 0x01);
+    delay(20);
+    transferData(0x0C, 0x01);
+    delay(20);
+    transferData(0x0B, 0x07);
+    delay(20);
+    transferData(0x0A, 0x0F);
+    delay(20);
+    */
+    transferData(0x09, 0x0F);
+    delay(20);
+    transferData(0x08, 0x0F);
+    delay(20);
+    transferData(0x07, 0x0F);
+    delay(20);
+    transferData(0x06, 0x0F);
+    delay(20);
+    transferData(0x05, 0x0F);
+    delay(20);
+    transferData(0x04, 0x0F);
+    delay(20);
+    transferData(0x03, 0x0F);
+    delay(20);
+    transferData(0x02, 0x0F);
+    delay(20);
+    transferData(0x01, 0x0F);
+    delay(20);
+    transferData(0x00, 0x0F);
+    return;
+  }
+  else if(selectedAddr == 'X' || selectedAddr == 'x'){
+    //Turn off all leds
+    sendMessage("Turn off all leds");
+    /*
+    transferData(0x0F, 0x01);
+    delay(20);
+    transferData(0x0C, 0x01);
+    delay(20);
+    transferData(0x0B, 0x07);
+    delay(20);
+    transferData(0x0A, 0x00);
+    */
+    delay(20);
+    transferData(0x09, 0x00);
+    delay(20);
+    transferData(0x08, 0x00);
+    delay(20);
+    transferData(0x07, 0x00);
+    delay(20);
+    transferData(0x06, 0x00);
+    delay(20);
+    transferData(0x05, 0x00);
+    delay(20);
+    transferData(0x04, 0x00);
+    delay(20);
+    transferData(0x03, 0x00);
+    delay(20);
+    transferData(0x02, 0x00);
+    delay(20);
+    transferData(0x01, 0x00);
+    delay(20);
+    transferData(0x00, 0x00);
+    delay(20);
+    return;
+  }
+
+  //convert the user input to hex
+  int addr = convertUserInput(selectedAddr);
 
   //execute the user command
-  switch(option){
-    case 'W':
+  switch(addr){  
+
+    case 0:
+      //No-Op
+      sendMessage("No-Op");
+      transferData(addr, 0x00);
+
+      break;
+
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
       
-      sendMessage("Waking up the max7219");
-      transferData(0x0C, 0x01);
+        //Digit
+        sendMessage("Digit " + String(addr-1));
+        Serial.println("Select the segment to turn on/off (0 (min) - 7 (max)):");
 
-      break;
-    case 'S':
-
-      sendMessage("Shutting down the max7219");
-      transferData(0x0C, 0x00);
-
-      break;
-    case 'T':
-
-      sendMessage("Entering in the test mode");
-      transferData(0x0F, 0x01);
-
-      break;
-    case 'N':
-
-      sendMessage("Entering in the normal mode");
-      transferData(0x0F, 0x00);
-
-      break;
-    case 'M':
-
-      sendMessage("Manual mode");
-    
-      //read the address
-      Serial.println("Please enter the address:");
-      while(!Serial.available());
-      address = Serial.read();
-
-      delay(500);
-
-      //read the data
-      Serial.println("Please enter the data:");
-      while(!Serial.available());
-      data = Serial.read();
-
-      transferData(convertUserInput(address), convertUserInput(data));
-
-      break;
-
-    case 'O':
-
-      sendMessage("Turning on all the leds");
-      for(int i = 1; i <= 8; i++){
-        transferData(i, 0x0F);
-        delay(1000);
-      }
-      
-      break;
-
-    case 'F':
-      
-      sendMessage("Turning off all the leds");
-
-      for(int i = 1; i <= 8; i++){
-
-        transferData(i, 0x00);
-        delay(1000);
-
-      }
+        while(!Serial.available())
+          ;
   
+        //Code block to create a local scope and avoid variable name conflicts
+        {
+          char selectedData = Serial.read();
+  
+          int data = convertUserInput(selectedData);
+  
+          if (data >= 0x00 && data <= 0x0F){
+            Serial.println("Digit value: " + String(data));
+            transferData(addr, data);
+          }
+          else{
+            Serial.println("Invalid option");
+          }
+  
+        }
+  
+        break;
+
+    case 9:
+      //Decode Mode
+      sendMessage("Decode Mode");
+      //(D0 - D1 - D2 - D3 - D4 - D5 - D6 - D7)
+      //Options:
       
+      // 1 - No decode for digits 0-7
+      // 2 - Code B decode for digit 0 No decode for digits 7–1
+      // 2 - Code B decode for digits 3–0 No decode for digits 7–4
+      // 3 - Code B decode for digits 0-7
+
+      Serial.println("Select the decode mode:");
+      Serial.println("1 - No decode for digits 0-7");
+      Serial.println("2 - Code B decode for digit 0 No decode for digits 7–1");
+      Serial.println("3 - Code B decode for digits 3–0 No decode for digits 7–4");
+      Serial.println("4 - Code B decode for digits 0-7");
+
+      while(!Serial.available())
+        ;
+
+      //Code block to create a local scope and avoid variable name conflicts
+      {
+        char selectedData = Serial.read();
+
+        if (selectedData == '1'){
+          Serial.println("No decode for digits 0-7");
+          transferData(addr, 0x00);
+          operationMode = 1;
+        }
+        else if (selectedData == '2'){
+          Serial.println("Code B decode for digit 0 No decode for digits 7–1");
+          transferData(addr, 0x01);
+          operationMode = 2;
+        }
+        else if (selectedData == '3'){
+          Serial.println("Code B decode for digits 3–0 No decode for digits 7–4");
+          transferData(addr, 0x0F);
+          operationMode = 3;
+        }
+        else if (selectedData == '4'){
+          Serial.println("Code B decode for digits 0-7");
+          transferData(addr, 0xFF);
+          operationMode = 4;
+        }
+        else{
+          Serial.println("Invalid option");
+        }
+
+      }
+
       break;
 
-    case 'D':
-        
-      sendMessage("No decode mode");
-      transferData(0x09, 0x00);
+    case 0x0A:
+      //Intensity
+      sendMessage("Intensity");
+      Serial.println("Select the intensity level (0 (min) - F (max)):");
+
+      while(!Serial.available())
+        ;
+
+      //Code block to create a local scope and avoid variable name conflicts
+      {
+        char selectedData = Serial.read();
+
+        int data = convertUserInput(selectedData);
+
+        if (data >= 0x00 && data <= 0x0F){
+          Serial.println("Intensity level: " + String(data));
+          transferData(addr, data);
+        }
+        else{
+          Serial.println("Invalid option");
+        }
+
+      }
 
       break;
 
-    case 'I':
+    case 0x0B:
+      //Scan Limit
+      sendMessage("Scan Limit");
+      Serial.println("Select the scan limit (0 (min) - 7 (max)):");
 
-      sendMessage("Minimum intensity");
-      transferData(0x0A, 0x00);
+      while(!Serial.available())
+        ;
+
+      //Code block to create a local scope and avoid variable name conflicts
+      {
+        char selectedData = Serial.read();
+
+        int data = convertUserInput(selectedData);
+
+        if (data >= 0x00 && data <= 0x07){
+          Serial.println("Scan limit: " + String(data));
+          transferData(addr, data);
+        }
+        else{
+          Serial.println("Invalid option");
+        }
+
+      }
 
       break;
 
-    case 'H':
+    case 0x0C:
 
-      sendMessage("Maximum intensity");
-      transferData(0x0A, 0x0F);
+      //Shutdown
+      sendMessage("Shutdown");
+      Serial.println("Select the shutdown mode:");
+      Serial.println("0 - Shutdown mode");
+      Serial.println("1 - Normal operation");
+
+      while(!Serial.available())
+        ;
+
+      //Code block to create a local scope and avoid variable name conflicts
+      {
+        char selectedData = Serial.read();
+
+        if (selectedData == '0'){
+          Serial.println("Shutdown mode");
+          transferData(addr, 0x00);
+        }
+        else if (selectedData == '1'){
+          Serial.println("Normal operation");
+          transferData(addr, 0x01);
+        }
+        else{
+          Serial.println("Invalid option");
+        }
+
+      }
 
       break;
 
-    case 'L':
+    case 0x0F:
 
-      sendMessage("Scan limit");
-      transferData(0x0B, 0x07);
-   
+      //Display Test
+      sendMessage("Display Test");
+      Serial.println("Select the display test mode:");
+      Serial.println("0 - Display test mode");
+      Serial.println("1 - Normal operation");
+
+      while(!Serial.available())
+        ;
+
+      //Code block to create a local scope and avoid variable name conflicts
+      {
+        char selectedData = Serial.read();
+
+        if (selectedData == '0'){
+          Serial.println("Display test mode");
+          transferData(addr, 0x00);
+        }
+        else if (selectedData == '1'){
+          Serial.println("Normal operation");
+          transferData(addr, 0x01);
+        }
+        else{
+          Serial.println("Invalid option");
+        }
+
+      }
+
       break;
+
+    
+
+    
 
     default:
     
